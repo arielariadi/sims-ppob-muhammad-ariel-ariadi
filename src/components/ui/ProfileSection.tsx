0d2cf00e2fbffa972e4078c319979ac0d831ea5c
Muhammad Ariel Ariadi <ariel.ariadi123@gmail.com>
2025-02-16 09:22:07 +0700

    feat: create profile section component

---
+++ src/components/ui/ProfileSection.tsx
@@ -0,0 +1,71 @@
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from './card';
+import { Button } from './button';
+import { Eye, EyeOff } from 'lucide-react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../app/store';
+import { toggleShowBalance } from '../../features/home/showBalanceSlice';
+import { UserBalanceType, UserProfileType } from '../../features/home/Types';
+
+type ProfileSectionProps = {
+	profilePhoto: string;
+	profile: UserProfileType | null;
+	balance: UserBalanceType | null;
+};
+
+export function ProfileSection({
+	profilePhoto,
+	profile,
+	balance,
+}: ProfileSectionProps) {
+	const showBalance = useSelector(
+		(state: RootState) => state.showBalance.showBalance
+	);
+	const dispatch = useDispatch<AppDispatch>();
+
+	return (
+		<div className="bg-white p-6 rounded-lg shadow-md mb-6">
+			<div className="flex flex-col md:flex-row items-center md:items-start gap-6">
+				<div className="w-full md:w-1/2 flex flex-col items-center md:items-start text-center md:text-left">
+					<img
+						src={profilePhoto}
+						alt={profile?.first_name}
+						className="w-24 h-24 rounded-full object-cover shadow-md"
+					/>
+					<h1 className="text-xl text-gray-600 font-medium mt-3">
+						Selamat datang,
+					</h1>
+					<h2 className="text-3xl font-bold">
+						{profile?.first_name} {profile?.last_name}
+					</h2>
+				</div>
+
+				<div className="w-full md:w-1/2">
+					<Card className="text-white bg-red-500 shadow-none border-none">
+						<CardHeader>
+							<CardTitle>Saldo Anda</CardTitle>
+						</CardHeader>
+						<CardContent>
+							<p className="font-bold text-2xl">
+								Your balance: Rp{' '}
+								{showBalance ? balance?.toLocaleString() : '*******'}
+							</p>
+						</CardContent>
+						<CardFooter className="flex items-center">
+							<p>Lihat saldo</p>
+							<Button
+								variant="ghost"
+								onClick={() => dispatch(toggleShowBalance())}
+								className="cursor-pointer hover:bg-transparent w-1">
+								{showBalance ? (
+									<EyeOff className="w-5 h-5" />
+								) : (
+									<Eye className="w-5 h-5" />
+								)}
+							</Button>
+						</CardFooter>
+					</Card>
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,31 +1,17 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../app/store';
-import {
-	userProfileThunk,
-	userBalanceThunk,
-	userServicesThunk,
-	userBannersThunk,
-} from '../features/home/homeThunks';
 import { profilePhoto } from '../assets';
 import {
-	Card,
-	CardContent,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from '../components/ui/card';
+	userBalanceThunk,
+	userBannersThunk,
+	userProfileThunk,
+	userServicesThunk,
+} from '../features/home/homeThunks';
 
-import { toggleShowBalance } from '../features/home/showBalanceSlice';
-
-import { Eye, EyeOff } from 'lucide-react';
-import { Button } from '../components/ui/button';
+import { ProfileSection } from '../components/ui/ProfileSection';
 
 export default function HomePage() {
-	const showBalance = useSelector(
-		(state: RootState) => state.showBalance.showBalance
-	);
-
 	const dispatch = useDispatch<AppDispatch>();
 	const { profile, balance, services, banners, loading, error } = useSelector(
 		(state: RootState) => state.home
@@ -44,59 +30,18 @@ export default function HomePage() {
 	return (
 		<div className="min-h-screen bg-gray-100">
 			<div className="container mx-auto p-4">
-				<div className="bg-white p-6 rounded-lg shadow-md mb-6">
-					<div className="flex flex-col md:flex-row items-center md:items-start gap-6">
-						{/* Bagian Profil */}
-						<div className="w-full md:w-1/2 flex flex-col items-center md:items-start text-center md:text-left">
-							<img
-								src={profilePhoto}
-								alt={profile?.first_name}
-								className="w-24 h-24 rounded-full object-cover shadow-md"
-							/>
-							<h1 className="text-md text-gray-600 font-medium mt-3">
-								Selamat datang,
-							</h1>
-							<h2 className="text-2xl font-bold">
-								{profile?.first_name} {profile?.last_name}
-							</h2>
-						</div>
-
-						{/* Bagian Saldo */}
-						<div className="w-full md:w-1/2">
-							<Card className="text-white bg-red-500 shadow-none border-none">
-								<CardHeader>
-									<CardTitle>Saldo Anda</CardTitle>
-								</CardHeader>
-								<CardContent>
-									<p className="font-bold text-2xl">
-										Your balance: Rp{' '}
-										{showBalance ? balance?.toLocaleString() : '*******'}
-									</p>
-								</CardContent>
-								<CardFooter className="flex items-center">
-									<p>Lihat saldo</p>
-									<Button
-										variant="ghost"
-										onClick={() => dispatch(toggleShowBalance())}
-										className="cursor-pointer hover:bg-transparent w-1">
-										{showBalance ? (
-											<EyeOff className="w-5 h-5" />
-										) : (
-											<Eye className="w-5 h-5" />
-										)}
-									</Button>
-								</CardFooter>
-							</Card>
-						</div>
-					</div>
-				</div>
+				<ProfileSection
+					profilePhoto={profilePhoto}
+					profile={profile}
+					balance={balance}
+				/>
 
 				{/* Services Section */}
 				<div className="bg-white p-6 rounded-lg shadow-md mb-6">
 					<div className="grid grid-cols-2 md:grid-cols-3 lg:flex lg:justify-center gap-4">
-						{services.map((service, index) => (
+						{services.map(service => (
 							<div
-								key={index + 1}
+								key={service.service_code}
 								className="text-center mx-auto md:w-24 md:h-24">
 								<img
 									src={service.service_icon}
@@ -113,10 +58,10 @@ export default function HomePage() {
 				<div className="bg-white p-6 rounded-lg shadow-md">
 					<h2 className="text-xl font-bold mb-4">Temukan Promosi Menarik</h2>
 					<div className="flex overflow-x-auto space-x-4">
-						{banners.map((banner, index) => (
+						{banners.map(banner => (
 							<>
 								<img
-									key={index + 1}
+									key={banner.banner_name}
 									src={banner.banner_image}
 									alt={banner.banner_name}
 									className="h-40 rounded-lg"
